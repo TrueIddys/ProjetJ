14110a10d03d5f8887e62ac12a8e2d263b12e0bd
TrueIddys <maxime.peyral@gmail.com>
2015-04-25 18:31:25 +0200

    modes de jeux mis en places + synchronisation des jscrollpanes

--- src/controleur/Utilities.java
+++ src/controleur/Utilities.java
@@ -3,6 +3,7 @@ package controleur;
 import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 
+
 /**
  * Created by Maxime on 24/03/2015.
  */
@@ -14,7 +15,7 @@ public class Utilities {
     public static void initFenetre(JFrame fenetre, JPanel contentPane){
         fenetre.setContentPane(contentPane);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setPreferredSize(new DimensionUIResource(600, 400));
+        fenetre.setPreferredSize(new DimensionUIResource(800, 600));
         fenetre.pack();
         fenetre.setLocationRelativeTo(null);
         fenetre.setVisible(true);
